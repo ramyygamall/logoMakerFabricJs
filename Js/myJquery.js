c7116e60b3eff699686089b205f28eda5676f5fe
Ramy Gamal <r4myy.gamall@gmail.com>
2017-09-14 14:12:43 +0200

    Update myJquery.js

--- Js/myJquery.js
+++ Js/myJquery.js
@@ -1,4 +1,17 @@
 
+//github
+if (document.addEventListener) {
+    document.addEventListener('contextmenu', function(e) {
+        alert("You've tried to open context menu"); //here you draw your own menu
+        e.preventDefault();
+    }, false);
+} else {
+    document.attachEvent('oncontextmenu', function() {
+        alert("You've tried to open context menu");
+        window.event.returnValue = false;
+    });
+}
+
 //  Downloading Image
 $('#saveImg').on('click',saveImg);
 function saveImg(){    
